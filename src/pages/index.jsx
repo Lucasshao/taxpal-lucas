78d33d729c91b4466c6bc2d16be9dc21d1023cd2
Lucasshao <yilongshao2020@gmail.com>
2024-02-24 11:08:14 +1100

    create Button component and Hero page

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,8 +1,14 @@
 import React from "react";
+import Head from "@/components/Head";
+import Hero from "@/components/Hero";
 
 const Home = () => {
-  return;
-  <></>;
+  return (
+    <>
+      <Head />
+      <Hero />
+    </>
+  );
 };
 
 export default Home;
